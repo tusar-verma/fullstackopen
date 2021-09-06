5d7fc208dde4e23a8d83a50381b18169e8fd23fc
tusar-verma <tusarverma12345@gmail.com>
2021-09-05 22:26:20 -0300

    Done up to exercise 2.10 of part2/thephonebook

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,14 +1,40 @@
 import React, { useState } from 'react'
 
+const Search = ({evHandler}) => (
+  <p>Search: <input onChange={evHandler}/></p>
+)
+
+const Form = (props) => (
+  <form>
+    <div>
+      name: <input value={props.newName} onChange={props.newNameInput}/>
+    </div>
+    <div>
+      number: <input value={props.newNumber} onChange={props.newNumberInput}/>
+    </div>
+    <div>
+      <button type="submit" onClick={props.addPerson} >add</button>
+    </div>
+  </form>
+)
+
+const Persons = (props) => (
+  props.personsToShow.map((person) => <p key={person.name}>{person.name} - {person.number}</p>)
+)  
+
+
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { 
-      name: 'Arto Hellas', 
-      number: '000'
-    }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ filter, setFilter] = useState('')
+
+  const personsToShow = filter === '' ? persons : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   const newNameInput = (event) => {
     setNewName(event.target.value)
@@ -16,6 +42,10 @@ const App = () => {
 
   const newNumberInput = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const searchEventHandler = (event) => {
+    setFilter(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -32,19 +62,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={newNameInput}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={newNumberInput}/>
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson} >add</button>
-        </div>
-      </form>
+        <Search evHandler={searchEventHandler}/>
+      <h2>Add new person</h2>
+        <Form newName={newName}  newNameInput={newNameInput}
+              newNumber={newNumber} newNumberInput={newNumberInput}
+              addPerson={addPerson}
+        />
       <h2>Numbers</h2>
-        {persons.map( (person) => <p key={person.name}>{person.name} - {person.number}</p>)}
+        <Persons personsToShow={personsToShow} />
     </div>
   )
 }
